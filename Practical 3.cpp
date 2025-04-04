5933d936d8ac1112730dccdfd922e37999b81c1f
zainalamzidan <zainzidan600@gmail.com>
2025-04-04 20:23:11 +0500

    Add files via upload

---
+++ Practical 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    int choice = 2;
+    
+	switch (choice) {
+    case 1:
+        cout << "Choice is 1\n";
+        break;
+    case 2:
+        cout << "Choice is 2\n";
+        break;
+    default:
+        cout << "Invalid choice\n";
+    }
+   
+   
+    return 0;
+}
