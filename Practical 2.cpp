f680b6102ca491db44168d399e230bf21083d79b
zainalamzidan <zainzidan600@gmail.com>
2025-04-04 20:17:20 +0500

    Add files via upload

---
+++ Practical 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num = 20;
+
+    if (num > 10) {
+        if (num % 2 == 0)
+            cout << "Number is even and greater than 10\n";
+    }
+
+
+    return 0;
+}
